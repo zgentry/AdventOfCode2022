7a536832ec58e00219c77af6ccb069ff3a849761
Zeb Gentry <gentry.zeb123@gmail.com>
2022-12-02 18:38:38 -0500

    Day 2 Part 2

--- Day02/input.js
+++ Day02/input.js
@@ -10,23 +10,33 @@ fs.readFile('Input.txt', 'utf-8', (err, data) => {
     matchOutcomes = [];
 
     matches.forEach(matchInput => {
-        let match = {score: 0, opponentPlay: 0, myPlay: 0};
+        let match = {score: 0, opponentPlay: 0, myPlay: 0, outcome: ''};
         matchPlay = matchInput.split(' ');
 
         if (matchPlay[0] == 'A') match.opponentPlay = 1;
         else if (matchPlay[0] == 'B') match.opponentPlay = 2;
         else if (matchPlay[0] == 'C') match.opponentPlay = 3;
 
-        if (matchPlay[1] == 'X') match.myPlay = 1;
-        else if (matchPlay[1] == 'Y') match.myPlay = 2;
-        else if (matchPlay[1] == 'Z') match.myPlay = 3;
+        if (matchPlay[1] == 'X') match.outcome = 'LOSE';
+        else if (matchPlay[1] == 'Y') match.outcome = 'TIE';
+        else if (matchPlay[1] == 'Z') match.outcome = 'WIN';
 
+        if (match.outcome == 'TIE') {
+            match.score += 3;
+            match.myPlay = match.opponentPlay;
+        } 
+        else if(match.outcome == 'LOSE') {
+            if (match.opponentPlay > 1) match.myPlay = match.opponentPlay - 1;
+            else match.myPlay = 3;
+        } 
+        else if(match.outcome == 'WIN') {
+            match.score +=6;
+            if (match.opponentPlay < 3) match.myPlay = match.opponentPlay + 1;
+            else match.myPlay = 1;
+        } 
+ 
         match.score += match.myPlay;
 
-        if (match.opponentPlay - match.myPlay == 0) match.score += 3;
-        else if(match.myPlay - match.opponentPlay == 1) match.score +=6;
-        else if(match.opponentPlay - match.myPlay == 2) match.score +=6;
- 
         matchOutcomes.push(match);
     });
 
