07f752951b23647b184a7de4265e7afb2f9cd0b6
Zeb Gentry <gentry.zeb123@gmail.com>
2022-12-06 07:30:57 -0500

    Day 06 Part 2

--- Day06/input.js
+++ Day06/input.js
@@ -5,6 +5,7 @@ fs.readFile('Input.txt', 'utf-8', (err, data) => {
 
     console.log(data);
 
+    //PART 1
     firstMatch = 0;
     for (i = 3; i < data.length; i++) {
         currentChars = data[i - 3] + data[i - 2] + data[i - 1] + data[i];
@@ -14,5 +15,19 @@ fs.readFile('Input.txt', 'utf-8', (err, data) => {
         }
     }
 
+    //PART 2
+    part2firstMatch = 0;
+    for (i = 13; i < data.length; i++) {
+        currentChars = '';
+        for (j = 14; j > 0; j--) {
+            currentChars += data[i - j + 1];
+        }
+        if (currentChars.match(/(?:([a-z])(?!.*\1)){14}/)) {
+            part2firstMatch = i + 1;
+            break;
+        }
+    }
+
     console.log(firstMatch);
+    console.log(part2firstMatch);
 });
