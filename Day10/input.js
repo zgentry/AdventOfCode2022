2bf05b11ef9e8807bff42105a970a626134ea9c1
Zeb Gentry <gentry.zeb123@gmail.com>
2022-12-10 08:57:41 -0500

    Day 10 Part 2

--- Day10/input.js
+++ Day10/input.js
@@ -5,14 +5,20 @@ fs.readFile('Input.txt', 'utf-8', (err, data) => {
 
     instructions = data.split('\r\n');
     xReg = [1]
+    crtMap = '';
     for (instruction of instructions) {
         [instruction, value] = instruction.split(' ');
         value = parseInt(value);
         console.log(`Instruction ${instruction} and value ${value}`);
+        if (xReg[xReg.length - 1] >= (xReg.length - 2) % 40 && xReg[xReg.length - 1] <= (xReg.length) % 40) crtMap += '#'
+        else crtMap += '.'
         xReg.push(xReg[xReg.length - 1]);
         if (instruction == "addx") {
+            if (xReg[xReg.length - 1] >= (xReg.length - 2) % 40 && xReg[xReg.length - 1] <= (xReg.length) % 40) crtMap += '#'
+            else crtMap += '.'
             xReg.push(xReg[xReg.length - 1] + value)
         }
+        if ((xReg.length - 1) % 40 == 0) crtMap += '\n'
     }
     console.log(xReg)
     signalStrengthSum = 0;
@@ -21,4 +27,5 @@ fs.readFile('Input.txt', 'utf-8', (err, data) => {
         console.log(`past 2: ${xReg[i - 2]} ${xReg[i - 1]} current: ${xReg[i]} next two ${xReg[i + 1]} ${xReg[i + 2]}`);
     }
     console.log(`The sum of the signal strength is ${signalStrengthSum}`)
+    console.log(crtMap);
 });
